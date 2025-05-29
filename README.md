1af8f5460c58a7fc9a4f9c3633ad7a87d20703ad
Alex <1inboxna@gmail.com>
2025-05-29 11:19:35 +0300

    start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# interview tracker
+# 2️⃣0️⃣2️⃣5️⃣ interview tracker
+
+# 💾 git push -u origin main
---
+++ src/components/login-google-form.jsx
@@ -0,0 +1,9 @@
+export const LoginGoogleForm = () => {
+  return (
+    <>
+      <div>
+        <Form className="my-10 mx-auto w-[1000px] px-2 text-sm py-2 shadow-md bg-[var(--main-bg-color)] "></Form>
+      </div>
+    </>
+  );
+};
